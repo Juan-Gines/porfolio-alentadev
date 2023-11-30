64a8f3f84a4fc7cce3f9b22f81a3fb4cbe8e8b68
Juan-Gines <JuangAlenta@hotmail.com>
2023-11-30 11:41:01 +0100

    Primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="p-10">
+      <h1 className="text-2xl md:text-4xl text-orange-300"> Juan Ginés Alenta Rodriguez</h1>
+      <p className="font-medium">Desarrollador web Full Stack</p>
+    </header>
+  )
+}
+
+export default Header
